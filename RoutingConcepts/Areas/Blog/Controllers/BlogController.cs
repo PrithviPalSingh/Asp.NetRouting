b7f4d95760de571e81a809c806e21e86436c8dac
Prithvi Pal Singh <Prithvi.Coer@outlook.com>
2017-12-01 11:57:29 +0800

    Master commit

---
+++ RoutingConcepts/Areas/Blog/Controllers/BlogController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RoutingConcepts.Areas.Blog.Controllers
+{
+    public class BlogController : Controller
+    {
+        //
+        // GET: /Blog/Blog/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ShowRecent()
+        {
+            return View();
+        }
+
+        public ActionResult ShowArchive()
+        {
+            return View();
+        }
+	}
+}
---
+++ RoutingConcepts/Areas/Dashboard/Controllers/DashboardController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RoutingConcepts.Areas.Dashboard.Controllers
+{
+    public class DashboardController : Controller
+    {
+        //
+        // GET: /Dashboard/Dashboard/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddPost()
+        {
+            return View();
+        }
+
+        public ActionResult EditPost()
+        {
+            return View();
+        }
+    }
+}
