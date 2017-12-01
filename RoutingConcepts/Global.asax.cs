16e97e286ab5ffdd46eccbec6a48928516d3e0b7
Prithvi Pal Singh <Prithvi.Coer@outlook.com>
2017-12-01 11:34:42 +0800

    Commit first

---
+++ RoutingConcepts/Global.asax.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace RoutingConcepts
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static string[] ViewLocation
+        {
+            get
+            {
+                return new[] { "~/Areas/Schemes/Views/{0}.cshtml", "~/Areas/Schemes/Views/{1}/{0}.cshtml",
+                "~/Areas/Services/Views/{0}.cshtml", "~/Areas/Services/Views/{1}/{0}.cshtml",
+                "~/Areas/Misc/Views/{0}.cshtml", "~/Areas/Misc/Views/{1}/{0}.cshtml",
+                "~/Areas/blog/Views/{0}.cshtml", "~/Areas/blog/Views/{1}/{0}.cshtml",
+                "~/Areas/dashboard/Views/{0}.cshtml", "~/Areas/dashboard/Views/{1}/{0}.cshtml"};
+            }
+        }
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            var razorEngine = ViewEngines.Engines.OfType<RazorViewEngine>().First();
+            razorEngine.ViewLocationFormats = razorEngine.ViewLocationFormats.Concat(ViewLocation).ToArray();
+        }
+    }
+}
